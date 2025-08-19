5ecddeca0e796454adf6bf9f72613c284db6debe
Hezekiah Ajayi-Omoleye <hezekiahomoleye@gmail.com>
2025-08-19 13:59:07 -0800

    Python Code used for NUORBIT Backend

--- Python_backend.py
+++ Python_backend.py
@@ -1,6 +1,7 @@
 # this is a simple python project aimed to impplement my practical knowledge in Backend
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+import datetime as date_time
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'# database URI   //// is an absolute path to the database file and /// is a relative path of that data base
@@ -11,7 +12,7 @@ db = SQLAlchemy(app) # SQLAlchemy is an ORM (Object Relational Mapper) that allo
 class Todo:(db.Model)
 id = db.Column(db.Integer, primary_key=True)
 content = db.Column(db.String(200), nullable=False) # We dont want the user to create a todo task and leaveit empty.
-
+date_time = db.column(db.DateTime) # This will automatically set the date and time when the task is created.
 def __repr__(self):
         return f'<Task {self.id}>' # This will return the id of the task when printed
 
