fcdcd6106071aa0dbf2d358956ff1d130a00be70
Hezekiah Ajayi-Omoleye <hezekiahomoleye@gmail.com>
2025-08-20 09:11:49 -0800

    Python Backend for NUORBIT

--- Python_backend.py
+++ Python_backend.py
@@ -1,5 +1,5 @@
 # this is a simple python project aimed to impplement my practical knowledge in Backend
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import datetime as date_time
 
@@ -18,6 +18,21 @@ content = db.Column(db.String(200), nullable=False) # We dont want the user to c
 date_time = db.column(db.DateTime) # This will automatically set the date and time when the task is created.
 def __repr__(self):
         return f'<Task {self.id}>' # This will return the id of the task when printed
+
+
+@app.route("/join", methods=["GET", "POST"])
+def join():
+    if request.method == "POST":
+        # Here you would handle the form submission, e.g., save to database
+        name = request.form.get("name")
+        email = request.form.get("email")
+        # You can add logic to save the name and email to the database or perform other actions
+    return "Thank you for joining!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
 @app.route("/")
 def home():
