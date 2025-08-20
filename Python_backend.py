7df90813703efad6600bfbbbfcf2adcf48b16f4b
Hezekiah Ajayi-Omoleye <hezekiahomoleye@gmail.com>
2025-08-20 11:26:35 -0800

    Nuorbit Backend Code

--- Python_backend.py
+++ Python_backend.py
@@ -4,40 +4,37 @@ from flask_sqlalchemy import SQLAlchemy
 import datetime as date_time
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'# database URI   //// is an absolute path to the database file and /// is a relative path of that data base
-
-#Data Base
-db = SQLAlchemy(app) # SQLAlchemy is an ORM (Object Relational Mapper) that allows us to interact with the database using Python objects instead of SQL queries.
-# At this point above, we initialize the database with the Flask app.
-
-
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)  # Id is the primary key of the table, which means it will be unique for each task. Think f it as features of the website.
-    content = db.Column(db.String(200), nullable=False) # We dont want the user to create a todo task and leaveit empty.
-    date_time = db.Column(db.DateTime) # This will automatically set the date and time when the task is created.
-    def __repr__(self):
-        return f'<Task {self.id}>' # This will return the id of the task when printed
-
 
 @app.route("/")
 def home():
+
     return render_template("index.html")   # must match filename
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
-
 
 @app.route("/join", methods=["GET", "POST"])
 def join():
     if request.method == "POST":
         name = request.form.get("name")
         email = request.form.get("email")
+        return redirect("/thank-you")
+    return '''
+        <form method="post">
+            Name: <input type="text" name="name" required><br>
+            Email: <input type="email" name="email" required><br>
+            <input type="submit" value="Join">
+        </form>
+    '''
+
+@app.route("/thank-you")
+def thank_you():
     return "Thank you for joining!"
-    # return redirect("/")
+
+
+
+
+@app.route("/play-video")
+def play_video():
+    return render_template("video.html")
+
+
 if __name__ == "__main__":
-
-
-    app.run(debug=True)
+    app.run(host = "0.0.0.0", port =5000, debug=True)
