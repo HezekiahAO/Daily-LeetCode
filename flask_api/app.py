e5e70198021263d9762b2aae8dbc646aa3b588f9
Hezekiah Ajayi-Omoleye <hezekiahomoleye@gmail.com>
2025-09-08 16:06:25 -0800

    Flask home route code

--- flask_api/app.py
+++ flask_api/app.py
@@ -2,29 +2,27 @@ from flask import Flask                                                         
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLAlchemy_DATABASE_URL'] = 'sqlite:///database.db'  # Set up your database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # Set up your database
 db = SQLAlchemy(app)
 from flask_restful import Resource, Api, reqparse, fields, marshal_with, abort                                                # SET UP STAGE
 api = Api(app)   # Thats our Api getting setup
 
 
 class UserModel(db.Model):   #Creates a database model for users
-    id = db.column(db.integer, primary_key=True)
-    name = db.column(db.string(100), Unique=True, nullable=False)
-    email = db.column(db.string(100), Unique=True, nullable = False)                                                           # SET UP CONTENTS AND CONDITIONS
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)                                                           # SET UP CONTENTS AND CONDITIONS
                                                                                                                                 
-def __repr__(self):    # A representational File
-    return f'User(name ={self.name}, email = {self.name})'   # A uniue represntation of a user
+    def __repr__(self):    # A representational File
+        return f'User(name ={self.name}, email = {self.email})'   # A uniue represntation of a user
 
 user_args = reqparse.RequestParser()
 user_args.add_argument('name', type = str, required = True, help = 'Name cannot be left Blank chief')
 user_args.add_argument('email', type = str, required = True, help = 'Email cannot be left Blank chief')
                     
 @app.route("/")   # / gives the home page. TThat right after them come
-
-
 def home():
     return "Hello Hezekiah, How was your day?"
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(debug=True)
