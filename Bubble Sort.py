71e6eeefd27adf680833dca9d7a22afad4e93505
Hezekiah Ajayi-Omoleye <hezekiahomoleye@gmail.com>
2026-04-10 12:35:44 +0100

    Bubble sort optimization to reduce no of passes

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -2,7 +2,14 @@ class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         n = len(nums)
         for i in range (n):
-            for j in range (n-1):
+            for j in range (n-1-i):
                 if nums[j] > nums[j+1]:
-                  nums[j], nums[j+1] = nums[j+1], nums[j]
+                   nums[j], nums[j+1] = nums[j+1], nums[j]
+                   swapped = True
+            if not swapped:
+                break
         return nums
+
+# Input: nums = [5,2,3,1]
+#Output: [1,2,3,5]
+#Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), while the positions of other numbers are changed (for example, 1 and 5).
